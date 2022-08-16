74d1a04e11676408cd9a14585c1fc6e3bcea2101
semameral <kara.kubilay@gmail.com>
2022-08-16 10:40:27 +0100

    first request

--- AutoSearch.Web/src/app/search/search.component.ts
+++ AutoSearch.Web/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+  _response: ResponseDto | undefined;
+  search(text: string) {
 
+    console.log(`Search`);
+    console.log(text);
+
+    let res = this.getResult(text).subscribe((data: ResponseDto) => {
+      this._response = { ...data }
+      console.log("this._response-1");
+      console.log(this._response);
+      if (this._response.isSuccess)
+        alert(this._response.result);
+    });
+  }
+  getResult(text: string) {
+    const params = new HttpParams().set('text', text).set('num', 100);
+
+    const resp = this.http.get<ResponseDto>(`${environment.AutoSearchApi}/Search`, { params });
+    console.log("resp");
+    console.log(resp);
+    return resp;
+  }
+}
+
+export interface ResponseDto {
+  isSuccess: boolean
+  result: {}
+  displayMessage: string
+  errorMessaages: [string]
 }
